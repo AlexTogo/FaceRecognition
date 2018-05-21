4dbd7e698e92330ab87dcd975b930a37c7cc3925
AlexTogo <>
2018-05-21 02:28:13 -0400

    added Node Server

--- src/App.js
+++ src/App.js
@@ -44,6 +44,13 @@ class App extends Component {
 		}
 	}
 
+	// componentDidMount() {
+	// 	fetch('http://localhost:3000')
+	// 		.then(response => response.json())
+	// 		.then(data => console.log(data))
+	// }
+
+
 	calculateFaceLocations = (data) => {
 		const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
 		const image = document.getElementById('inputImage')
--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,21 +1,59 @@
 import React from 'react'
 import './SignIn.css';
 
-const SignIn = ({ onRouteChange }) => {
-	return (
-		<div className="SignIn-container textShadow">
-			<h1>Welcome</h1>
-			<form className="SignIn-form">
-				<input type="text" placeholder="Username" />
-				<input type="password" placeholder="Password" />
-				<button type="submit" id="login-button" onClick={() => onRouteChange('home')}>Login</button>
+
+class SignIn extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			signInEmail: '',
+			signInPassword: ''
+		}
+	}
+
+	onEmailChange = (event) => {
+		this.setState({ signInEmail: event.target.value });
+	}
+
+	onPasswordChange = (event) => {
+		this.setState({ signInPassword: event.target.value })
+	}
+
+	onSubmitSignIn = () => {
+		fetch('http://localhost:3000/signin', {
+			method: 'post',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({
+				email: this.state.signInEmail,
+				password: this.state.signInPassword
+			})
+		})
+			.then(response => response.json())
+			.then(data => {
+				if (data === 'success') {
+					this.props.onRouteChange('home');
+				}
+			});
+	}
+
+	render() {
+		const { onRouteChange } = this.props;
+		return (
+			<div className="SignIn-container textShadow" >
+				<h1>Welcome</h1>
+				{/* <form className="SignIn-form"> */}
+				<input type="text" placeholder="Username" onChange={this.onEmailChange} />
+				<input type="password" placeholder="Password" onChange={this.onPasswordChange} />
+				<button
+					type="submit"
+					onClick={this.onSubmitSignIn}>Login</button>
 				<p
 					className="register"
-					onClick={() => onRouteChange('register')}
-				>Register</p>
-			</form>
-		</div>
-	)
+					onClick={() => onRouteChange('register')}>Register</p>
+				{/* </form> */}
+			</div>
+		)
+	}
 }
 
 export default SignIn
